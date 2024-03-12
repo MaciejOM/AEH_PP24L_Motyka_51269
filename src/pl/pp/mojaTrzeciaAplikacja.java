561aadbc6f551144286ddf4190a802d9af56e4a8
S204-030\student <jasinm4919584_aehit@students.vizja.pl>
2024-03-12 12:55:22 +0100

    Pierwszy zapis do repozytorium Lab3

---
+++ src/pl/pp/mojaTrzeciaAplikacja.java
@@ -0,0 +1,91 @@
+package pl.pp;
+import java.util.Scanner;
+
+public class mojaTrzeciaAplikacja {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        //Zadanie 2.1
+
+        while(true) {
+            System.out.println("Podaj liczbę dni: ");
+            var dni = scanner.nextInt();
+
+            if(dni < 0) {
+                System.out.println("Wyjście...");
+                break;
+            }
+            System.out.println(dni + " dni to " + dni / 7 + " tygodnie i " + dni % 7 + " dni");
+        }
+
+
+        //Zadanie 2.2
+
+        while(true) {
+            System.out.println("Podaj temperaturę w Fahrenheit'ach: ");
+            double fahr = scanner.nextDouble();
+            double celcius = (fahr - 32.0) * 1.8;
+            double kelvin = celcius + 273.16;
+
+            System.out.println("Stopnie Celcjusza: " + String.format("%.2f", celcius));
+            System.out.println("Stopnie Fahrenheit'a: " + String.format("%.2f", fahr));
+            System.out.println("Stopnie Kelvina: " + String.format("%.2f",kelvin));
+            if(fahr == -1){
+                System.out.println("Wyjście...");
+                break;
+            }
+
+        }
+
+        //Zadanie 1
+
+      /*  System.out.println("Podaj proszę liczbę większą od 100:");
+
+        var number1 = scanner.nextDouble();
+        while (number1 <= 100) {
+            System.out.println("Liczba nie jest większa od 100, podaj jeszcze raz:");
+            number1 = scanner.nextDouble();
+        }
+        System.out.println("Dziękuje! Podałeś liczbę: " + number1);
+
+        double number2;
+        do {
+            System.out.println("Podaj proszę liczbę większą od 200");
+            number2 = scanner.nextDouble();
+        } while (number2 <= 200);
+        System.out.println("Dziękuję! Podałeś Liczbę: " + number2);
+
+        int wynik=0;
+        for (var i = 1; i <= 10; i++) {
+            wynik = wynik + i;
+            System.out.println("Przebieg numer " + i + " w pętli for, a zmienna wynik = " + wynik);
+        }
+
+        System.out.println("Podaj proszę liczbę x: ");
+        var x = scanner.nextDouble();
+        System.out.println("Podaj proszę liczbę y: ");
+        var y = scanner.nextDouble();
+
+        if(x > y) {
+            System.out.println("x jest większe od y");
+        } else if (x < y) {
+            System.out.println("x jest mniejsze od y");
+        } else {
+            System.out.println("x jest równe y");
+        }
+        while(true)
+        {
+            System.out.println("Wpisz wartość -1 żeby wyjść z programu");
+            var input = scanner.nextDouble();
+            if(input == -1){
+                System.out.println("Wyjście...");
+                break;
+            }
+        }
+        scanner.close();
+        */
+
+
+    }
+}
